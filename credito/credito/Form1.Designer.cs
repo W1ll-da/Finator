3741110540c90fafd29196bdd58f87d0c936e175
W1ll-da <149895601+W1ll-da@users.noreply.github.com>
2023-11-14 23:39:00 -0500

    Add files via upload

---
+++ credito/credito/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace credito
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewTabla = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrestamo = new System.Windows.Forms.TextBox();
+            this.txtTasaInteres = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPeriodos = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtCuota = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCuotaAbono = new System.Windows.Forms.TextBox();
+            this.btnRecalcularAbono = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAbono = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewTabla
+            // 
+            this.dataGridViewTabla.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridViewTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTabla.Location = new System.Drawing.Point(174, 6);
+            this.dataGridViewTabla.Name = "dataGridViewTabla";
+            this.dataGridViewTabla.Size = new System.Drawing.Size(598, 339);
+            this.dataGridViewTabla.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Valor del prestamo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Valor de interes \r\n";
+            // 
+            // txtPrestamo
+            // 
+            this.txtPrestamo.Location = new System.Drawing.Point(15, 25);
+            this.txtPrestamo.Name = "txtPrestamo";
+            this.txtPrestamo.Size = new System.Drawing.Size(100, 20);
+            this.txtPrestamo.TabIndex = 4;
+            // 
+            // txtTasaInteres
+            // 
+            this.txtTasaInteres.Location = new System.Drawing.Point(15, 62);
+            this.txtTasaInteres.Name = "txtTasaInteres";
+            this.txtTasaInteres.Size = new System.Drawing.Size(100, 20);
+            this.txtTasaInteres.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Numero de cuotas";
+            // 
+            // txtPeriodos
+            // 
+            this.txtPeriodos.Location = new System.Drawing.Point(15, 104);
+            this.txtPeriodos.Name = "txtPeriodos";
+            this.txtPeriodos.Size = new System.Drawing.Size(100, 20);
+            this.txtPeriodos.TabIndex = 8;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(15, 137);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 23);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtCuota
+            // 
+            this.txtCuota.Location = new System.Drawing.Point(15, 179);
+            this.txtCuota.Name = "txtCuota";
+            this.txtCuota.Size = new System.Drawing.Size(100, 20);
+            this.txtCuota.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Valor de la Cuota";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Numero de la cuota a Abonar";
+            // 
+            // txtCuotaAbono
+            // 
+            this.txtCuotaAbono.Location = new System.Drawing.Point(15, 285);
+            this.txtCuotaAbono.Name = "txtCuotaAbono";
+            this.txtCuotaAbono.Size = new System.Drawing.Size(100, 20);
+            this.txtCuotaAbono.TabIndex = 13;
+            // 
+            // btnRecalcularAbono
+            // 
+            this.btnRecalcularAbono.Location = new System.Drawing.Point(15, 351);
+            this.btnRecalcularAbono.Name = "btnRecalcularAbono";
+            this.btnRecalcularAbono.Size = new System.Drawing.Size(100, 23);
+            this.btnRecalcularAbono.TabIndex = 14;
+            this.btnRecalcularAbono.Text = "Reajustar";
+            this.btnRecalcularAbono.UseVisualStyleBackColor = true;
+            this.btnRecalcularAbono.Click += new System.EventHandler(this.btnAbonar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 308);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Monto a Abonar";
+            // 
+            // txtAbono
+            // 
+            this.txtAbono.Location = new System.Drawing.Point(15, 325);
+            this.txtAbono.Name = "txtAbono";
+            this.txtAbono.Size = new System.Drawing.Size(100, 20);
+            this.txtAbono.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(839, 491);
+            this.Controls.Add(this.txtAbono);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnRecalcularAbono);
+            this.Controls.Add(this.txtCuotaAbono);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCuota);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPeriodos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTasaInteres);
+            this.Controls.Add(this.txtPrestamo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewTabla);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTabla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewTabla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrestamo;
+        private System.Windows.Forms.TextBox txtTasaInteres;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPeriodos;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtCuota;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCuotaAbono;
+        private System.Windows.Forms.Button btnRecalcularAbono;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAbono;
+    }
+}
+
---
+++ credito/credito/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace credito
+{
+    public partial class Form1 : Form
+    {
+        private double saldoInicialAntesDeExtraordinario;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Agregar columnas al DataGridView
+            dataGridViewTabla.ColumnCount = 5;
+            dataGridViewTabla.Columns[0].Name = "Numero de Cuota";
+            dataGridViewTabla.Columns[1].Name = "saldo Inicial";
+            dataGridViewTabla.Columns[2].Name = "Interes";
+            dataGridViewTabla.Columns[3].Name = "Abono Capital";
+            dataGridViewTabla.Columns[4].Name = "Saldo Final";
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtPrestamo.Text, out double prestamo) &&
+               double.TryParse(txtTasaInteres.Text, out double tasa) &&
+               int.TryParse(txtPeriodos.Text, out int periodos))
+            {
+                // Convertir la tasa de interés a porcentaje (si es ingresada como un decimal)
+                if (tasa > 1)
+                {
+                    tasa /= 100; // Convertir a porcentaje (0.035 a 3.5)
+                }
+
+                saldoInicialAntesDeExtraordinario = prestamo;
+
+                double cuota = CalcularCuota(prestamo, tasa, periodos);
+
+                txtCuota.Text = Math.Round(cuota, 0).ToString(); // Mostrar la cuota redondeada al entero más cercano
+                //txtCuota.Text = cuota.ToString("F2"); // Mostrar la cuota en el TextBox
+
+
+                GenerarTablaAmortizacion(prestamo, tasa, periodos, cuota);
+                
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese números válidos en los campos.");
+
+            }
+        }
+        
+
+        private double CalcularCuota(double prestamo, double tasa, int periodos)
+        {
+
+            double tasaDecimal = tasa; // Convertir la tasa a decimal si es necesario
+            double cuota = prestamo * (tasaDecimal / (1 - Math.Pow(1 + tasaDecimal, -periodos)));
+            return cuota;
+        }
+
+        private void GenerarTablaAmortizacion(double prestamo, double tasa, int periodos, double cuota)
+        {
+            dataGridViewTabla.Rows.Clear();
+
+            double saldoInicial = prestamo;
+            double tasaDecimal = tasa;
+
+            double totalCapital = 0;
+            int totalInteres = 0;
+
+            for (int i = 1; i <= periodos; i++)
+            {
+                double interes = saldoInicial * tasaDecimal;
+                double abonoCapital = cuota - interes;
+               
+                double saldoFinal = saldoInicial - abonoCapital;
+
+                dataGridViewTabla.Rows.Add(i, (int)saldoInicial, (int)interes, (int)abonoCapital, (int)saldoFinal);
+                totalCapital += abonoCapital;
+                totalInteres += (int)interes;
+
+                saldoInicial = saldoFinal;
+            }
+
+            // Agregar la fila con la suma de los totales
+            dataGridViewTabla.Rows.Add("Total", "", totalInteres, totalCapital, "");
+
+            // Establecer estilos visuales para resaltar los totales
+            dataGridViewTabla.Rows[dataGridViewTabla.RowCount - 1].DefaultCellStyle.Font = new System.Drawing.Font(dataGridViewTabla.Font, System.Drawing.FontStyle.Bold);
+            dataGridViewTabla.Rows[dataGridViewTabla.RowCount - 1].DefaultCellStyle.BackColor = System.Drawing.Color.LightGray;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void btnAbonar_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtAbono.Text, out double abono) &&
+        int.TryParse(txtCuotaAbono.Text, out int cuotaAplicar))
+            {
+                if (cuotaAplicar >= 1 && cuotaAplicar <= dataGridViewTabla.RowCount)
+                {
+                    AplicarAbonoExtraordinario(abono, cuotaAplicar);
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, ingrese un número de cuota válido.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un valor válido para el abono.");
+            }
+        }
+
+        private void AplicarAbonoExtraordinario(double abono, int cuotaAplicar)
+        {
+            // Obtener el saldo inicial antes del abono extraordinario
+            double saldoInicial = saldoInicialAntesDeExtraordinario;
+
+            // Calcular la cuota original
+            double tasa = Convert.ToDouble(txtTasaInteres.Text) / 100;
+            double cuotaOriginal = Convert.ToDouble(txtCuota.Text);
+            int periodosRestantes = Convert.ToInt32(txtPeriodos.Text) - cuotaAplicar + 1;
+
+            // Recalcular el saldo después del abono extraordinario
+            for (int i = 1; i <= periodosRestantes; i++)
+            {
+                double interes = saldoInicial * tasa;
+                double abonoCapital = cuotaOriginal - interes + abono;
+                double saldoFinal = saldoInicial - abonoCapital;
+
+                dataGridViewTabla.Rows[cuotaAplicar - 1].Cells[2].Value = (int)interes;
+                dataGridViewTabla.Rows[cuotaAplicar - 1].Cells[3].Value = (int)abonoCapital;
+                dataGridViewTabla.Rows[cuotaAplicar - 1].Cells[4].Value = (int)saldoFinal;
+
+                saldoInicial = saldoFinal;
+                cuotaAplicar++;
+            }
+        }
+
+
+
+    }
+}
+
+
+
+
+
+
+
